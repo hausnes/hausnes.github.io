cdce7fa15ca6af3d021f0a97b097cc56175fd069
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2021-02-08 09:26:59 +0100

    Oppdatert JS. Òg endringar i HTML.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,5 @@
 console.log("Test av JS.");
+let navn = prompt("Kva heiter du?");
+document.getElementById("navnFraaBrukar").innerHTML = navn;
+var tid = new Date();
+document.getElementById("tidspunkt").innerHTML = tid;
