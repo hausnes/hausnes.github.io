4293248e18ff43acf513d853b694453279eb46d0
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2021-03-16 16:51:33 +0100

    Fiksa feil, 'data' til 'data.results'

--- api/henteBrukarar/kodeAlternativ.js
+++ api/henteBrukarar/kodeAlternativ.js
@@ -1,17 +1,28 @@
-// NB: Fungerer ikkje per no!
+// Alternativ kode som gjer det same som "kode.js", men med same tilnærming som Studio Ghibli-eksempelet.
 
 var request = new XMLHttpRequest();
 request.open('GET', 'https://randomuser.me/api/?results=30', true);
 
-request.onload = function () {
+request.onload = function () { // Køyrer først når data er mottatt frå nettsida
 	// Begin accessing JSON data here
 	var data = JSON.parse(this.response);
+	console.log(data); // Test for å sjå kva data me får inn
+	
+	// For-løkke på vår "vanlige" måte
+	console.log("Utskrift 1:");
+	for(let bruker of data.results)
+	{
+		console.log(bruker.name.title + " " + bruker.name.last);
+	}
+
+	// For-løkke, alternativ
+	console.log("\nUtskrift v2:")
 	if (request.status >= 200 && request.status < 400) {
-		data.forEach((brukar) => {
+		data.results.forEach((brukar) => {
             console.log(brukar.name.title + " " + brukar.name.last);
   	})
   	} else {
-		console.log("Feilmelding!");
+		console.log("Data blei ikkje lasta inn frå ekstern kjelde.");
   	}
 }
 
